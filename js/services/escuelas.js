896516d3e1cd83b75dd47ed538adb886fc92ee01
PAntonioPH <59242335+PAntonioPH@users.noreply.github.com>
2022-11-04 15:05:25 -0600

    Servicio consultar escuelas

--- js/services/escuelas.js
+++ js/services/escuelas.js
@@ -4,7 +4,10 @@ const consultar_escuelas = async (req, res) => {
   const result = await pool.query("Select * From escuela;");
   res.send({
     message: result.rows,
+    sucess: true,
+    failure: false,
   });
+  res.sendStatus(200);
 };
 
 module.exports = {
