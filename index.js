2f21679793356b4de2b20f0ca49d5db0150474fe
PAntonioPH <antonio.paph1@gmail.com>
2022-11-10 09:31:36 -0600

    Pagina error 404

--- index.js
+++ index.js
@@ -62,14 +62,6 @@ const routes_session = (route, session_true, session_false, log_out) => (request
   }
 };
 
-//Carga de vistas
-app.get("/", routes_session("/", "dashboard.html", "login.html", false));
-app.get("/login.html", routes_session("/login.html", "dashboard.html", "login.html", false));
-app.get("/logout.html", routes_session("/out.html", "login.html", "login.html", true));
-app.get("/dashboard.html", routes_session("/dashboard.html", "dashboard.html", "login.html", false));
-app.get("/escuelas.html", routes_session("/escuelas.html", "escuelas.html", "login.html", false));
-app.get("/usuarios.html", routes_session("/usuarios.html", "usuarios.html", "login.html", false));
-
 //Escuelas
 app.post("/api/v1/escuelas/consultar_escuelas", consultar_escuelas);
 app.post("/api/v1/escuelas/consultar_escuela", consultar_escuela);
@@ -99,3 +91,19 @@ app.get("/api/v1/controles/consultar_controles", consultar_controles);
 
 //Usuario
 app.post("/api/v1/usuarios/consultar_usuario", consultar_usuario);
+
+
+//Carga de vistas
+app.get("/", routes_session("/", "dashboard.html", "login.html", false));
+app.get("/login.html", routes_session("/login.html", "dashboard.html", "login.html", false));
+app.get("/logout.html", routes_session("/out.html", "login.html", "login.html", true));
+app.get("/dashboard.html", routes_session("/dashboard.html", "dashboard.html", "login.html", false));
+app.get("/escuelas.html", routes_session("/escuelas.html", "escuelas.html", "login.html", false));
+app.get("/usuarios.html", routes_session("/usuarios.html", "usuarios.html", "login.html", false));
+
+app.use((request, response) => {
+  response.status(404);
+
+  console.log("Not found")
+  response.sendFile(__dirname + `/public/error-404.html`);
+});
