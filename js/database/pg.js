0c9e274cdf6bf3980fd6c5f3dd836a008fe214fe
PAntonioPH <antonio.paph1@gmail.com>
2022-11-17 18:07:49 -0600

    Corrección base de datos

--- js/database/pg.js
+++ js/database/pg.js
@@ -1,7 +1,13 @@
 const {Pool} = require("pg");
 
+const user = "postgres"
+const password = "HweyYZPAARqECOuuPlj6"
+const host = "containers-us-west-115.railway.app"
+const port = "7703"
+const database = "railway"
+
 const pool = new Pool({
-  connectionString: "postgres://postgres:HweyYZPAARqECOuuPlj6@containers-us-west-115.railway.app:7703/railway",
+  connectionString: `postgres://${user}:${password}@${host}:${port}/${database}`,
   ssl: {rejectUnauthorized: false},
 });
 
